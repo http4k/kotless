ef160e210ae0fcf6429587fe2b4138f1a7d230b2
David Denton <denton.david@gmail.com>
2020-10-26 17:55:56 +0000

    tidy main

--- dsl/http4k/http4k-lang-local/src/main/kotlin/io/kotless/local/http4k/Main.kt
+++ dsl/http4k/http4k-lang-local/src/main/kotlin/io/kotless/local/http4k/Main.kt
@@ -1,19 +1,21 @@
 package io.kotless.local.http4k
 
 import io.kotless.Constants
+import io.kotless.Constants.Local.KtorOrSpringOrHttp4k
 import io.kotless.dsl.http4k.Kotless
 import org.http4k.server.SunHttp
 import org.http4k.server.asServer
 import kotlin.reflect.full.primaryConstructor
 
 fun main() {
-    val port = System.getenv(Constants.Local.serverPort).toInt()
-    val classToStart = System.getenv(Constants.Local.KtorOrSpringOrHttp4k.classToStart)
+    val classToStart = System.getenv(KtorOrSpringOrHttp4k.classToStart)
 
     val ktClass = ::main::class.java.classLoader.loadClass(classToStart).kotlin
     val instance = (ktClass.primaryConstructor?.call() ?: ktClass.objectInstance) as? Kotless
 
     val kotless = instance ?: error("The entry point $classToStart does not inherit from ${Kotless::class.qualifiedName}!")
+
+    val port = System.getenv(Constants.Local.serverPort).toInt()
 
     kotless.handler().asServer(SunHttp(port)).start()
 }
--- dsl/http4k/http4k-lang-parser/src/main/kotlin/io/kotless/parser/http4k/Http4kDescriptor.kt
+++ dsl/http4k/http4k-lang-parser/src/main/kotlin/io/kotless/parser/http4k/Http4kDescriptor.kt
@@ -3,9 +3,9 @@ package io.kotless.parser.http4k
 import io.kotless.parser.DSLDescriptor
 
 object Http4kDescriptor : DSLDescriptor {
-    override val name: String = "http4k"
+    override val name = "http4k"
 
     override val parser = Http4kParser
 
-    override val localEntryPoint: String = "io.kotless.local.http4k.MainKt"
+    override val localEntryPoint = "io.kotless.local.http4k.MainKt"
 }
